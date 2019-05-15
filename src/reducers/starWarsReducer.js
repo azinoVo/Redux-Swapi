e99ac244102aefed872c71983565d674e8d26803
azinoVo <azinovo22@gmail.com>
2019-05-15 17:55:41 -0500

    added half reducer backbone

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,32 @@
-import /* we need our action types here*/ "../actions";
+import /* we need our action types here*/ { FETCHING_DATA, DATA_SUCCESS, DATA_FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
   // Array characters, Boolean fetching, null error.
+  loading: false,
+  error: null
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+      case FETCHING_DATA:
+        return {
+          // perform state change for these actions
+        };
+
+      case DATA_SUCCESS:
+        return {
+          // perform state change for these actions
+        };
+
+      case DATA_FAILURE:
+        return {
+          // perform state change for these actions
+        };
+
     default:
       return state;
   }
